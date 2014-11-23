0013f05b46aacd09fb8068349c8cb3ac9e926d15
Roman Nowicki <roman.nowicki@performgroup.com>
2014-11-23 19:48:12 +0100

    unittest for 'integer' assure

---
+++ tests/Integer.php
@@ -0,0 +1,78 @@
+<?php namespace Test\Assure\Integer;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    public function validData()
+    {
+        return [
+            [1, 1],
+            ['10', 10],
+        ];
+    }
+
+    /**
+     * @dataProvider validData
+     */
+    public function testValidData($given, $expected)
+    {
+        assure($given, 'integer');
+        $this->assertEquals($given, $expected);
+        $this->assertInternalType('integer', $expected);
+    }
+
+    public function invalidData()
+    {
+        return [
+            [null],
+            [1.1],
+            ['one'],
+            [false],
+            [[]],
+            [new \stdClass]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidData
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidData($given)
+    {
+        assure($given, 'integer');
+    }
+
+    public function testCheckConditionValid()
+    {
+        $given = 10;
+        assure($given, 'integer <= 10');
+        assure($given, 'integer > 9');
+        assure($given, 'integer >= 10');
+        assure($given, 'integer >= 1');
+        assure($given, 'integer < 11');
+        assure($given, 'integer == 10');
+        assure($given, 'integer != 0');
+        $this->assertInternalType('integer', $given);
+    }
+
+    public function invalidCondition()
+    {
+        return [
+            [10, 'integer <= 9'],
+            [10, 'integer > 10'],
+            [10, 'integer < 9'],
+            [10, 'integer != 10'],
+            [10, 'integer == 0'],
+            [10, 'integer >= 100'],
+            ['10', 'integer >= 100'],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidCondition
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCheckConditionInValid($given, $condition)
+    {
+        assure($given, $condition);
+    }
+}
