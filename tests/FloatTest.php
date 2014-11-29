45d3c4b35ee3cbec115f35ff57e2223cd2f16789
Roman Nowicki <roman.nowicki@performgroup.com>
2014-11-29 15:21:22 +0100

    unittest Float

---
+++ tests/FloatTest.php
@@ -0,0 +1,42 @@
+<?php namespace Test\Assure\Float;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    public function validData()
+    {
+        return [
+            [1.1, 1.1],
+            ['10.1', 10.1],
+        ];
+    }
+
+    /**
+     * @dataProvider validData
+     */
+    public function testValidData($given, $expected)
+    {
+        assure($given, 'float');
+        $this->assertEquals($given, $expected);
+        $this->assertInternalType('float', $expected);
+    }
+
+    public function invalidData()
+    {
+        return [
+            [null],
+            ['one'],
+            [false],
+            [[]],
+            [new \stdClass]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidData
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidData($given)
+    {
+        assure($given, 'float');
+    }
+}
