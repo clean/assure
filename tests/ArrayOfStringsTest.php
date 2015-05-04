86cee322f62f941c785a8afd5d7e478490178d25
Roman Nowicki <roman.nowicki@performgroup.com>
2015-05-04 09:54:48 +0000

    extend UnitTest for ArrayOfStrings

--- tests/ArrayOfStringsTest.php
+++ tests/ArrayOfStringsTest.php
@@ -6,7 +6,9 @@ class TestCase extends \PHPUnit_Framework_TestCase
     {
         return [
             ['a', ['a']],
+            ['-', ['-']],
             [['one', 'two'], ['one', 'two']],
+            [(object)['one', 'two'], ['one', 'two']],
         ];
     }
 
@@ -18,5 +20,25 @@ class TestCase extends \PHPUnit_Framework_TestCase
         assure($given, 'arrayOfStrings');
         $this->assertInternalType('array', $expected);
         $this->assertEquals($given, $expected);
+    }
+
+    public function invalidData()
+    {
+        return [
+            [[]],
+            [null],
+            [1],
+            [[1,'a']],
+            [(object)[1,'a']],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @dataProvider invalidData
+     */
+    public function testInValidData($given)
+    {
+        assure($given, 'arrayOfStrings');
     }
 }
