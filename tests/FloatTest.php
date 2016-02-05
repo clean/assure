ed8c2d9a6c85a242ea2e42f06c9a049ffa185d3d
Roman Nowicki <roman.nowicki@inbox.com>
2016-02-05 17:28:03 +0100

    extend test data samples in unit test for float assure

--- tests/FloatTest.php
+++ tests/FloatTest.php
@@ -6,6 +6,8 @@ class TestCase extends \PHPUnit_Framework_TestCase
     {
         return [
             [1.1, 1.1],
+            [1, 1.0],
+            ['1', 1.0],
             ['10.1', 10.1],
         ];
     }
